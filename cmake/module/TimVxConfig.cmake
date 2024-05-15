7acf2f2560401525ac249bd42276d721318d5ab7
xiang.zhang <xiang.zhang@verisilicon.com>
2024-05-15 22:38:18 +0800

    Refine project cmake configuration
    Inhouse-Tag:V1.1.0-240515
    
    Signed-off-by: xiang.zhang <xiang.zhang@verisilicon.com>

--- cmake/module/TimVxConfig.cmake
+++ cmake/module/TimVxConfig.cmake
@@ -23,36 +23,60 @@ if(NOT PUBLIC_TIM_VX)
     set(repo_addr git@gitlab-cn.verisilicon.com:npu_sw/verisilicon/tim-vx.git)
 endif()
 
+if(NOT TIM_VX_TAG)
+set(TIM_VX_TAG "main")
+endif()
+
 message(STATUS "use TIM_VX from ${repo_addr}")
+message(STATUS "TIM_VX version: ${TIM_VX_TAG}")
+message(STATUS "TIM_VX build with external sdk -> ${EXT_VIV_SDK}")
+
 if(USE_GRPC)
     ExternalProject_Add(tim-vx
         SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/deps/tim-vx
         GIT_REPOSITORY ${repo_addr}
-        GIT_TAG main
+        GIT_TAG ${TIM_VX_TAG}
         CMAKE_ARGS
             "-DTIM_VX_ENABLE_PLATFORM=ON"
-            "-DEXTERNAL_VIV_SDK=${CMAKE_SOURCE_DIR}/prebuilt/android_arm64"
-            "-DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/tim-vx-install/"
+            "-DEXTERNAL_VIV_SDK=${EXT_VIV_SDK}"
+            "-DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/tim-vx-install/${LIBABI}"
             "-DCMAKE_TOOLCHAIN_FILE:FILEPATH=${CMAKE_TOOLCHAIN_FILE}"
             "-DANDROID_ABI=${ANDROID_ABI}"
-            "-DTIM_VX_DBG_ENABLE_TENSOR_HNDL=OFF"
+            "-DTIM_VX_DBG_ENABLE_TENSOR_HNDL=${ENABLE_TENSOR_HANDLE}"
             "-DTIM_VX_ENABLE_GRPC=ON"
             "-DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}"
             "-DProtobuf_DIR=${Protobuf_DIR}"
             "-DgRPC_DIR=${gRPC_DIR}"
             "-Dabsl_DIR=${absl_DIR}"
+            "-DANDROID_PLATFORM=${ANDROID_PLATFORM}"
     )
 else()
+    list(APPEND ARGS "-DTIM_VX_ENABLE_PLATFORM=ON")
+    list(APPEND ARGS "-DEXTERNAL_VIV_SDK=${CMAKE_SOURCE_DIR}/prebuilt/android_arm64")
+    list(APPEND ARGS "-DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/tim-vx-install/")
+    list(APPEND ARGS "-DCMAKE_TOOLCHAIN_FILE:FILEPATH=${CMAKE_TOOLCHAIN_FILE}")
+    list(APPEND ARGS "-DANDROID_ABI=${ANDROID_ABI}")
+
+    if(NOT ANDROID_PLATFORM)
+        list(APPEND ARGS "-DANDROID_PLATFORM=${ANDROID_PLATFORM}")
+    else()
+        list(APPEND ARGS "-DANDROID_PLATFORM=android-34")
+    endif()
+
+    message(STATUS "TIM-VX cmake args = ${ARGS}")
+
     ExternalProject_Add(tim-vx
         SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/deps/tim-vx
         GIT_REPOSITORY ${repo_addr}
-        GIT_TAG main
+        GIT_TAG ${TIM_VX_TAG}
         CMAKE_ARGS
             "-DTIM_VX_ENABLE_PLATFORM=ON"
-            "-DEXTERNAL_VIV_SDK=${CMAKE_SOURCE_DIR}/prebuilt/android_arm64"
-            "-DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/tim-vx-install/"
+            "-DEXTERNAL_VIV_SDK=${EXT_VIV_SDK}"
+            "-DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/tim-vx-install/${LIBABI}"
             "-DCMAKE_TOOLCHAIN_FILE:FILEPATH=${CMAKE_TOOLCHAIN_FILE}"
             "-DANDROID_ABI=${ANDROID_ABI}"
-            "-DTIM_VX_DBG_ENABLE_TENSOR_HNDL=OFF"
+            "-DTIM_VX_DBG_ENABLE_TENSOR_HNDL=${ENABLE_TENSOR_HANDLE}"
+            "-DANDROID_PLATFORM=${ANDROID_PLATFORM}"
+            "-DCMAKE_INSTALL_LIBDIR=${CMAKE_INSTALL_LIBDIR}"
     )
 endif()
